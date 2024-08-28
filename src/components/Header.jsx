b674f668fef2cd3c6a526d25cc1d522a496a1208
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-28 16:44:43 -0300

    feat: header preset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 const Header = () => {
   return (  
     <div>
-      header
+      <nav className="flex justify-center items-center py-3">
+        <ul className="flex gap-5 items-center text-sm">
+          <li className="bg-type-gray px-5 py-1 rounded-full">
+            <a 
+              href="#" 
+              className="text-secondary hover:text-tertiary"
+            >
+                Home
+            </a>
+          </li>
+          <li><a href="#" className="text-secondary hover:text-tertiary">About</a></li>
+          <li><a href="#" className="text-secondary hover:text-tertiary">Projects</a></li>
+          <li><a href="#" className="text-secondary hover:text-tertiary">Contact</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
