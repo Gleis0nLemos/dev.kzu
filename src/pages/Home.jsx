8fe943e9dc93082314d39197107928497970c063
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-28 15:49:05 -0300

    feat: add links between layouts and routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from "../layouts/MainLayout";
+
+const Home = () => {
+  return ( 
+    <Layout>
+      Home
+    </Layout>
+   );
+}
+ 
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:  {
+        'primary': '#000', //#1E201E', '#191815'
+        'secondary': '#F7F9F2',
+        'tertiary': '#EEEEEE',
+        'link': '#4C3BCF',
+        'type-gray': '#31363F',
+        'lime': '#4CCD99' 
+      }
+    },
   },
   plugins: [],
 }
-
