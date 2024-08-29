c45d9fc85f9284c721f287b83cad6a8f3dbb92bf
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-29 00:17:34 -0300

    feat: contact page presetting

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -50,6 +50,17 @@ const Contact = () => {
             ))}
         </div>
       </div>
+      <div className="flex items-center gap-2 mt-24">
+        <h1 className="text-link">Contact</h1>
+        <div>
+          <hr className="w-[10px] border-t-2 border-link" />
+        </div>
+      </div>
+      <div>
+        <h1 className="text-2xl font-semibold">Look Who's Here</h1>
+        <p className="mb-1 mt-2 text-gray-400">The fastest way to get in touch with me is to send me a message on <span className="text-lime">LinkedIn</span> or a DM on <span className="text-lime">Instagram</span>.</p>
+        <p className="mb-1 mt-2 text-gray-400">Whether you have a question or just want to say "Hi," I'll try my best to get back to you.</p>
+      </div>
     </div>
   );
 }
