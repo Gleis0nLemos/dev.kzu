7c30a4cbaf68c09411f62959a09f2013837df646
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-29 18:55:01 -0300

    feat: home animated full setting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,56 +5,56 @@ const Home = () => {
     <div className="pt-12">
       <div className="container mx-auto mt-36 justify-start items-center max-w-3xl border-l-2 border-type-gray">
         <div className="flex ml-3 gap-1">
-          <span className="bg-gradient-1 h-2.5 w-[122px]"></span>
-          <span className="bg-light-gray h-2.5 w-[76px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px] rounded-full"></span>
+          <span className="bg-gradient-1 h-2.5 w-[122px] animate-fade-left animate-duration-300 animate-delay-300"></span>
+          <span className="bg-light-gray h-2.5 w-[76px] animate-fade-left animate-duration-300 animate-delay-500"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] rounded-full animate-fade-left animate-duration-300 animate-delay-700"></span>
         </div>
         <div className="flex mt-1.5 ml-3">
-          <span className="bg-gradient-3 h-2.5 w-[253px]"></span>
+          <span className="bg-gradient-3 h-2.5 w-[253px] animate-fade-left animate-duration-200ms animate-delay-[400ms]"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-4 h-2.5 w-[110px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px]"></span>
-          <span className="bg-light-gray h-2.5 w-[56px]"></span>
-          <span className="bg-gradient-1 h-2.5 w-[128px]"></span>
+          <span className="bg-gradient-4 h-2.5 w-[110px] animate-fade-left animate-duration-300 animate-delay-500"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] animate-fade-left animate-duration-300 animate-delay-700"></span>
+          <span className="bg-light-gray h-2.5 w-[56px] animate-fade-left animate-duration-300 animate-delay-[800ms]"></span>
+          <span className="bg-gradient-1 h-2.5 w-[128px] animate-fade-left animate-duration-300 animate-delay-[900ms]"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-1 h-2.5 w-[157px]"></span>
-          <span className="bg-light-gray h-2.5 w-[76px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px] rounded-full"></span>
-          <span className="bg-gradient-4 h-2.5 w-[110px]"></span>
+          <span className="bg-gradient-1 h-2.5 w-[157px] animate-fade-left animate-duration-300 animate-delay-200"></span>
+          <span className="bg-light-gray h-2.5 w-[76px] animate-fade-left animate-duration-300 animate-delay-[400ms]"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] rounded-full animate-fade-left animate-duration-300 animate-delay-[600ms]"></span>
+          <span className="bg-gradient-4 h-2.5 w-[110px] animate-duration-300 animate-fade-left animate-delay-700"></span>
         </div>
         <div className="flex mt-1.5 gap-1 ml-12">
-          <span className="bg-gradient-1 h-2.5 w-[92px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px]"></span>
-          <span className="bg-light-gray h-2.5 w-[76px]"></span>
-          <span className="bg-gradient-3 h-2.5 w-[113px]"></span>
+          <span className="bg-gradient-1 h-2.5 w-[92px] animate-fade-left animate-duration-300 animate-delay-300"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] animate-duration-300 animate-fade-left animate-delay-500"></span>
+          <span className="bg-light-gray h-2.5 w-[76px] animate-duration-300 animate-fade-left animate-delay-[600ms]"></span>
+          <span className="bg-gradient-3 h-2.5 w-[113px] animate-duration-300 animate-fade-left animate-delay-700"></span>
         </div>
         <div className="ml-12 my-4">
-          <h1 className="text-4xl font-semibold">KZu.dev</h1>
-          <p className="mt-2">Fullstack Developer</p>
+          <h1 className="text-4xl font-semibold animate-fade-left animate-duration-300 animate-delay-300">KZu.dev</h1>
+          <p className="mt-2 animate-fade-left animate-duration-300 animate-delay-500">Fullstack Developer</p>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-4 h-2.5 w-[152px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px] rounded-full"></span>
+          <span className="bg-gradient-4 h-2.5 w-[152px] animate-fade-left animate-duration-300 animate-delay-300"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] rounded-full animate-fade-left animate-duration-300 animate-delay-500"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-3 h-2.5 w-[99px]"></span>
-          <span className="bg-gradient-1 h-2.5 w-[72px]"></span>
-          <span className="bg-gradient-3 h-2.5 w-[172px]"></span>
+          <span className="bg-gradient-3 h-2.5 w-[99px] animate-fade-left animate-duration-300 animate-delay-300"></span>
+          <span className="bg-gradient-1 h-2.5 w-[72px] animate-fade-left animate-duration-300 animate-delay-500"></span>
+          <span className="bg-gradient-3 h-2.5 w-[172px] animate-fade-left animate-duration-300 animate-delay-700"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-1 h-2.5 w-[155px]"></span>
-          <span className="bg-gradient-3 h-2.5 w-[76px]"></span>
+          <span className="bg-gradient-1 h-2.5 w-[155px] animate-fade-left animate-duration-300 animate-delay-[400ms]"></span>
+          <span className="bg-gradient-3 h-2.5 w-[76px] animate-fade-left animate-duration-300 animate-delay-500"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-4 h-2.5 w-[179px]"></span>
+          <span className="bg-gradient-4 h-2.5 w-[179px] animate-fade-left animate-duration-300 animate-delay-500"></span>
         </div>
         <div className="flex mt-1.5 ml-3 gap-1">
-          <span className="bg-gradient-3 h-2.5 w-[76px]"></span>
-          <span className="bg-light-gray h-2.5 w-[10px]"></span>
-          <span className="bg-gradient-4 h-2.5 w-[76px]"></span>
-          <span className="bg-gradient-1 h-2.5 w-[42px]"></span>
+          <span className="bg-gradient-3 h-2.5 w-[76px] animate-fade-left animate-duration-300 animate-delay-300"></span>
+          <span className="bg-light-gray h-2.5 w-[10px] animate-fade-left animate-duration-300 animate-delay-500"></span>
+          <span className="bg-gradient-4 h-2.5 w-[76px] animate-fade-left animate-duration-300 animate-delay-700"></span>
+          <span className="bg-gradient-1 h-2.5 w-[42px] animate-fade-left animate-duration-300 animate-delay-[900ms]"></span>
         </div>
       </div>
     </div>
