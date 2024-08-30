5255a185eba410c542cbdb265d5a119fcfb05ede
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-30 13:03:25 -0300

    feat: add dinamic link in links box

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,20 +1,22 @@
 const ContactCard = ({ title, icon, socialLink }) => {
   return (
-    <div className={`px-3 py-4 flex flex-col font-semibold justify-between text-gray-400 border border-type-gray  border-b-2 border-b-lime hover:border-light-gray hover:border-b-lime`}>
-      <div className="flex justify-between items-center mx-1 my-1">
-        <a href={socialLink} target="_blank">
-          <h2 className="text-2xl">{icon}</h2>
-        </a>
-        <a
-          href={socialLink}
-          className="hover:underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {title}
-        </a>
+    <a href={socialLink} target="_blank">
+      <div className={`px-3 py-4 flex flex-col font-semibold justify-between text-gray-400 border border-type-gray  border-b-2 border-b-lime hover:border-light-gray hover:border-b-lime`}>
+        <div className="flex justify-between items-center mx-1 my-1">
+          <a href={socialLink} target="_blank">
+            <h2 className="text-2xl">{icon}</h2>
+          </a>
+          <a
+            href={socialLink}
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {title}
+          </a>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
